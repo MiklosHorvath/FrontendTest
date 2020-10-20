bf0ae48522a15dfe5b60821d0a086520869ded0f
MiklosHorvath <horvathmikloswork@gmail.com>
2020-10-20 23:12:31 +0200

    improve design

--- colorpicker/src/components/ColorPicker.js
+++ colorpicker/src/components/ColorPicker.js
@@ -2,6 +2,16 @@ import React, { useContext } from "react";
 import Grid from "@material-ui/core/Grid";
 import Palette from "./Palette";
 import PaletteContext from "../context/PaletteContext";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    root: {
+      width: "100vw",
+      height: "100vh",
+      margin: "0px",
+      padding: "0px"
+    },
+  });
 
 const ColorPicker = () => {
   const { palettes } = useContext(PaletteContext);  
--- colorpicker/src/components/Palette.js
+++ colorpicker/src/components/Palette.js
@@ -1,38 +1,42 @@
 import React from "react";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) =>({
+  mainBox: {
+    width: "80vw",
+    flexDirection: "column",
+    [theme.breakpoints.up('md')]: {
+    flexDirection: "row",
+    width: "40vw"
+    }
+  },
+}));
 
 const Palette = (props) => {
+  const classes = useStyles();
   return (
     <div>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
-          <Grid item xs={12} sm={12}>
-            <Box bgcolor="error.main" color="error.contrastText" p={2}>
+      <Box display="flex"  className={classes.mainBox}>
+        <Box display="flex" flexDirection="column">
+          <Box bgcolor="error.main" color="error.contrastText" p={2}>
             {props.paletteInfo.title}
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={12}>
-            <Box bgcolor="warning.main" color="warning.contrastText" p={2}>
-            by {props.paletteInfo.userName} at {props.paletteInfo.dateCreated}
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={12}>
-            <Box bgcolor="info.main" color="info.contrastText" p={2}>
-            views {props.paletteInfo.numViews} votes {props.paletteInfo.numVotes}
-            </Box>
-          </Grid>
-        </Grid>
-
-        <Grid item xs={12} sm={3}>
-          <Box p={3}>
-            <img
-              src={props.paletteInfo.imageUrl}
-              alt="palette image place"
-            ></img>
           </Box>
-        </Grid>
-      </Grid>
+
+          <Box bgcolor="warning.main" color="warning.contrastText" p={2}>
+            by {props.paletteInfo.userName} at {props.paletteInfo.dateCreated}
+          </Box>
+
+          <Box bgcolor="info.main" color="info.contrastText" p={2}>
+            views {props.paletteInfo.numViews} votes{" "}
+            {props.paletteInfo.numVotes}
+          </Box>
+        </Box>
+        <Box p={3}>
+          <img src={props.paletteInfo.imageUrl} alt="palette image place"></img>
+        </Box>
+      </Box>
     </div>
   );
 };
