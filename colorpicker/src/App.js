2567b4e7b0c2ae53d807ae42fbdf91e3fabf105e
MiklosHorvath <horvathmikloswork@gmail.com>
2020-10-20 15:01:29 +0200

    implements proof of concept for fetching data

--- colorpicker/src/App.js
+++ colorpicker/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+
+const HandleClickFetchColors = () => {
+  console.log("start fetching");
+  const options = {
+    url: "https://cors-anywhere.herokuapp.com/http://www.colourlovers.com/api/palettes/new?numResults=8&format=json",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",      
+    }
+  }
+    axios(options).then((resp) => {
+      console.log("response:");
+      console.log(resp);      
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <button onClick={HandleClickFetchColors}>FetchColors</button>
     </div>
   );
 }
